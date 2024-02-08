6eee0dbed8a72e41f2dac7234a3a96b3dd5c47fd
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-08 23:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+  //write your code here
+ 
+  let minimumCost = 0;
+ 
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+ 
+    const smallestA = arr.shift();
+    const smallestB = arr.shift();
+ 
+    const length = smallestA + smallestB;
+    minimumCost += length;
+ 
+    arr.push(length);
+  }
+ 
+  return minimumCost;
 }
-
-module.exports=mincost;
+ 
+module.exports = mincost;
